dac4532952cb561ea0207dec16503546567d1926
Justin Ahmann <jahmanndev@gmail.com>
2021-04-24 14:18:24 -0400

    (showing balance updates - Justin) Updating dom elements to show balance on transaction

--- index.js
+++ index.js
@@ -1,7 +1,36 @@
 let form = document.getElementById("transaction-form")
-form.onsubmit = (e) => {
-    console.log(e.target["transaction-type"].value)
-    console.log(e.target.amount.value)
+let balanceAmount = document.getElementById("account-balance")
 
+let balance = 0.00
+
+form.onsubmit = (e) => {
+   
+    let type;
+    if (e.target["transaction-type"].value === "1") type = "Deposit";
+    else type = "Withdraw"; 
+
+    let transaction = {
+        type: type ,
+        amount: Number(e.target.amount.value)
+    }
+
+    // console.log(transaction)
+    handleTransaction(transaction)
+    renderBalance()
     e.preventDefault()
+}
+
+function handleTransaction(transaction){
+    console.log(transaction) 
+    console.log(transaction.type) 
+    if (transaction.type === "Deposit"){
+        balance += transaction.amount
+    } else {
+        balance -= transaction.amount
+    }
+    console.log(balance) 
+}
+
+function renderBalance(){
+    balanceAmount.innerText = `$ ${balance}`
 }
